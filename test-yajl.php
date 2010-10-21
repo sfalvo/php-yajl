be7d74b3538d24c89381eed464eed5d090a445a6
Sam Falvo <sfalvo@Sam-Falvos-MacBook-Pro.local>
2010-10-21 10:42:02 -0700

    Verified context is passed opaquely.

--- test-yajl.php
+++ test-yajl.php
@@ -4,7 +4,7 @@ function test_yajl_null($ctx) {
     print "test_yajl_null called with context $ctx\n";
 }
 
-$hYajl = yajl_new(0, 0, null, test_yajl_null);
+$hYajl = yajl_new(0, 0, "SMACKDOWN", test_yajl_null);
 print "hYajl = ".$hYajl."\n";
 
 $T = "{ \"foo\": null }";
